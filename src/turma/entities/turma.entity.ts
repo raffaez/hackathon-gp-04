fe6d665638d3343fb108b5e1ec95ecc4bc2a23a8
raffaez <rafaeleasouzas@gmail.com>
2023-01-11 21:09:28 -0300

    chore: remove unused import

--- src/turma/entities/turma.entity.ts
+++ src/turma/entities/turma.entity.ts
@@ -1,5 +1,4 @@
 import { IsNotEmpty } from 'class-validator';
-import { isNotEmpty } from 'class-validator/types/decorator/decorators';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'tb_turma' })
