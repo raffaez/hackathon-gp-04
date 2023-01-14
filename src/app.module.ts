2bbfeeb418b98fc340638399f4e575b02f9d35fd
raffaez <rafaeleasouzas@gmail.com>
2023-01-14 18:11:45 -0300

    deploy prep

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,15 +11,26 @@ import { AppController } from './app.controller';
 
 @Module({
   imports: [
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: '14253500',
+    //   database: 'db_hackathon',
+    //   entities: [Turma, GrupoPi, Projeto],
+    //   synchronize: true,
+    // }),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '14253500',
-      database: 'db_hackathon',
-      entities: [Turma, GrupoPi, Projeto],
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      logging: false,
+      dropSchema: false,
+      ssl: {
+        rejectUnauthorized: false,
+      },
       synchronize: true,
+      autoLoadEntities: true,
     }),
     TurmaModule,
     GrupoPiModule,
